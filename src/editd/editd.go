b974bc037c953a3c7b6cfb634c7d859129466e8f
Thibaut Havel <thibaut.havel@gmail.com>
2016-03-20 17:31:14 +0100

    Dead simple etcd uri builder.

--- src/editd/editd.go
+++ src/editd/editd.go
@@ -1,9 +1,30 @@
 package main
 
 import (
-    "log"
+	"log"
+	"fmt"
+	"strings"
 )
 
+var (
+	etcdApi  = "v2"
+	etcdAddr = "etcd"
+	etcdPort = 4001
+)
+
+const (
+	endpoint = "http://%s:%d/%s/keys/%s"
+)
+
+func etcdUrl(keyPath string) string {
+	if strings.HasSuffix(keyPath, "/") {
+		panic(fmt.Sprintf("'%s' is not a valid key!", keyPath))
+	}
+
+	keyPath = strings.TrimPrefix(keyPath, "/")
+	return fmt.Sprintf(endpoint, etcdAddr, etcdPort, etcdApi, keyPath)
+}
+
 func main() {
-	log.Println("hello world")
+	log.Println(etcdUrl("/hello/world"))
 }
